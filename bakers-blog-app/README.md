2a52fedea7bf755210bdc2d154926c28af4aa81c
Donna Amos <donnacamos.developer@gmail.com>
2019-07-27 00:03:43 +0000

    modified README

---
+++ bakers-blog-app/README.md
@@ -0,0 +1,3 @@
+# Baker's Blog 
+
+Welcome to Baker's Blog. Clone the SSH key into your terminal, run `bundle install` and then run `shotgun` to gain access to the IP address. Once you have the IP address, copy and paste to your browser. Then create an account by clicking the Sign Up button. Keep your favorite baking recipes on this app and record what you did on the content section. Happy baking and bon appetite! 
---
+++ bakers-blog-app/app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class Posts < ApplicationController 
+  
+  # get posts/new 
+  
+   get '/posts/new' do 
+     erb :'/posts/new' 
+   end 
+   
+   post '/posts' do 
+    raise params.inspect 
+    
+    
+    if !logged_in
+      redirect to '/'
+    end 
+    
+    if params[:posts] != ""
+      @post = Post.create(posts: params[:content], recipes: params[:recipes], user_id: current_user.id) 
+      redirect to '/posts/#{@post.id}' 
+    else 
+      redirect to '/posts/show' 
+    end 
+    
+    get '/posts/:id' do  
+      set_post  
+      erb :'/posts/show'   
+    end 
+   end
+   
+     get '/posts/:id/edit' do 
+       set_post 
+       if logged_in?
+         if @post.user = current_user 
+           erb :'/posts/edit' 
+         else
+           redirect to '/posts/#{current_user.id}'
+       else 
+         redirect to '/'
+       end 
+     end 
+         
+     end 
+     
+     patch '/posts/:id' do 
+       set_post
+      if logged_in?
+         if @post.user = current_user 
+           erb :'/posts/edit' 
+         else
+           redirect to '/posts/#{current_user.id}'
+       else 
+         redirect to '/'
+       end  
+     end 
+     
+     get set_post 
+       @post = Post.find(params[:id])
+     end 
+   
+end 
